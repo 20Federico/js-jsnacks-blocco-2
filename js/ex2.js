64b52c78804daecd232663970359415bbc85efc6
20Federico <fefe.molino@gmail.com>
2021-10-15 16:55:42 +0200

    ex 2

---
+++ js/ex2.js
@@ -0,0 +1,15 @@
+
+/*
+Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi all’array che ha meno elementi
+fino a quando ne avrà tanti quanti l’altro.
+*/
+
+const array1 = [15, 2, 7];
+const array2 = [5];
+
+for (i = 0; i = (array1.length - array2.length); i++) {
+  
+  array2.push(' ');
+}
+console.log(array2);
