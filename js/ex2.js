65ea300edc92bc93adbd396b5eadfd82eae3204c
20Federico <fefe.molino@gmail.com>
2021-10-15 16:58:09 +0200

    comments added

--- js/ex2.js
+++ js/ex2.js
@@ -5,11 +5,14 @@ Aggiungi elementi all’array che ha meno elementi
 fino a quando ne avrà tanti quanti l’altro.
 */
 
+// creo variabili degli array 
 const array1 = [15, 2, 7];
 const array2 = [5];
 
+
+// cicla un numero di volte pari alla differenza tra array1 e array2
 for (i = 0; i = (array1.length - array2.length); i++) {
-  
+  // aggiungi all'array2 un numero di elementi pari alla differenza tra il numero di elementi in array1 e gli elementi in array2
   array2.push(' ');
 }
 console.log(array2);
